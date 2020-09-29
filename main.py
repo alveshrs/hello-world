2bf3a16efa6a06a2cb23c4419853099bc12eb869
Henrique Alves <henrique.rsalves@outlook.com>
2020-09-29 15:05:52 -0300

    adicionando corte

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import recorte as rc
 
 file = r'C:\Users\henri\Desktop\apostas.xlsx'
 df_bens = pd.read_excel(file)
 
 print(len(df_bens))
-print(df_bens.head(5))
+
+new_df = rc.corte(df_bens,10,20)
+
+print(len(new_df))
---
+++ recorte.py
@@ -0,0 +1,3 @@
+def corte(df, indice_start, indice_end):
+    df = df[indice_start:indice_end]
+    return df
